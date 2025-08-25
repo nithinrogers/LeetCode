0802ad6d1c3c625a15d0826ca0824a1f66851331
Nithin S <nithinsathish05@gmail.com>
2025-08-25 17:37:20 +0530

    Create 409. Longest Palindrome.java

---
+++ 409. Longest Palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char ch : s.toCharArray()) {
+            count[ch]++;
+        }
+        int length = 0;
+        boolean oddFound = false;
+        for (int c : count) {
+            if (c % 2 == 0) {
+                length += c;
+            } else {
+                length += c - 1;
+                oddFound = true;
+            }
+        }
+        if (oddFound) length++;
+        return length;
+    }
+}
